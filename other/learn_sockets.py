d105daf5c9985cb6a78eb3b394142baa313f7d08
wangxiaoyang <fengzhongdewojy@126.com>
2018-04-02 12:15:19 +0800

    add sorted and socket

---
+++ other/learn_sockets.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import socket
+import threading
+import socketserver
+
+client_addr = []
+class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
+
+    #准备请求处理
+    def setup(self):
+        ip = self.client_address[0].strip()     # 获取客户端的ip
+        port = self.client_address[1]           # 获取客户端的port
+        print(ip+":"+str(port)+" is connect!")
+        client_addr.append(self.client_address) # 保存到队列中
+
+    #处理请求
+    def handle(self):
+        data = str(self.request.recv(1024), 'ascii')
+        cur_thread = threading.current_thread()
+        response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
+        self.request.sendall(response)
+
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+def client(ip, port, message):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((ip, port))
+        sock.sendall(bytes(message, 'ascii'))
+        response = str(sock.recv(1024), 'ascii')
+        print("Received: {}".format(response))
+
+if __name__ == "__main__":
+    # Port 0 means to select an arbitrary unused port
+    HOST, PORT = "localhost", 0
+
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    ip, port = server.server_address
+
+    # Start a thread with the server -- that thread will then start one
+    # more thread for each request
+    server_thread = threading.Thread(target=server.serve_forever)
+    # Exit the server thread when the main thread terminates
+    server_thread.daemon = True
+    server_thread.start()
+    print("Server loop running in thread:", server_thread.name)
+
+    client(ip, port, "Hello World 1")
+    client(ip, port, "Hello World 2")
+    client(ip, port, "Hello World 3")
+
+    server.shutdown()
+    server.server_close()
