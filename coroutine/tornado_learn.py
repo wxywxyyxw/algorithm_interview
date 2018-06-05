ed7f4c7d67e0fe79bcaad5aff3512fd934612df2
wangxiaoyang <fengzhongdewojy@126.com>
2018-06-05 10:01:44 +0800

    add yield

--- coroutine/tornado_learn.py
+++ coroutine/tornado_learn.py
@@ -51,6 +51,7 @@
 #     url_list = [ 'http://baidu.com/','https://www.bing.com']
 #     http_generator_way(*url_list)
 #     tornado.ioloop.IOLoop.instance().start()
+from collections import namedtuple
 
 def test_hello():
     import asyncio
@@ -120,9 +121,142 @@ def test_basic_yield():
     res = next(gen)
     print ("res is %s" % res)
 
+
+def test_yield_return():
+    """
+    python3.3之后生成器中才可以使用return
+    """
+
+
+    Result = namedtuple('Result', 'count average')
+
+    def averager():
+        total = 0.0
+        count = 0
+        average = None
+        while True:
+            term = yield
+            if term is None:
+                break  # 为了返回值，协程必须正常终止；这里是退出条件
+            total += term
+            count += 1
+            average = total/count
+        # 返回一个namedtuple，包含count和average两个字段。在python3.3前，如果生成器返回值，会报错
+        return Result(count, average)
+
+    coro_avg = averager()
+    next(coro_avg)
+    coro_avg.send(20)
+    coro_avg.send(30)
+    coro_avg.send(40)
+
+    #coro_avg.send(None) # 发送None终止循环，导致协程结束。生成器对象会抛出StopIteration异常。异常对象的value属性保存着返回值。
+    try:
+         coro_avg.send(None)
+    except StopIteration as exc:
+        result = exc.value # 利用try except 来接收return的值
+    print (result)
+
+def  yield_from_simple_use():
+    # def gen():
+    #     for c in 'AB':
+    #         yield c
+    #     for i in range(1, 3):
+    #         yield i
+
+    #利用yield from 可以简化yield的写法
+    def gen():
+        yield from 'AB'
+        yield from range(1, 3)
+    print (list(gen()))
+
+def yield_from_flatten():
+    from collections import Iterable
+
+    def flatten(items, ignore_types=(str, bytes)):
+        for x in items:
+            if isinstance(x, Iterable) and not isinstance(x, ignore_types):
+                yield from flatten(x) # 这里递归调用，如果x是可迭代对象，继续分解
+            else:
+                yield x
+
+    items = [1, 2, [3, 4, [5, 6], 7], 8]
+
+    # Produces 1 2 3 4 5 6 7 8
+    for x in flatten(items):
+        print(x)
+
+    items = ['Dave', 'Paula', ['Thomas', 'Lewis']]
+    for x in flatten(items):
+        print(x)
+
+
+def use_yield_complex():
+    Result = namedtuple('Result', 'count average')
+
+    # 子生成器
+    # 这个例子和上边示例中的 averager 协程一样，只不过这里是作为字生成器使用
+    def averager():
+        total = 0.0
+        count = 0
+        average = None
+        while True:
+            # main 函数发送数据到这里
+            term = yield
+            if term is None: # 终止条件
+                break
+            total += term
+            count += 1
+            average = total/count
+        return Result(count, average) # 返回的Result 会成为grouper函数中yield from表达式的值
+
+
+    # 委派生成器
+    def grouper(results, key):
+         # 这个循环每次都会新建一个averager 实例，每个实例都是作为协程使用的生成器对象
+        while True:
+            # grouper 发送的每个值都会经由yield from 处理，通过管道传给averager 实例。grouper会在yield from表达式处暂停，等待averager实例处理客户端发来的值。averager实例运行完毕后，返回的值绑定到results[key] 上。while 循环会不断创建averager实例，处理更多的值。
+            results[key] = yield from averager()
+
+
+    # 调用方
+    def main(data):
+        results = {}
+        for key, values in data.items():
+            # group 是调用grouper函数得到的生成器对象，传给grouper 函数的第一个参数是results，用于收集结果；第二个是某个键
+            group = grouper(results, key)
+            group.send(None) #等价于next(group)
+            for value in values:
+                # 把各个value传给grouper 传入的值最终到达averager函数中；
+                # grouper并不知道传入的是什么，同时grouper实例在yield from处暂停
+                group.send(value)
+            # 把None传入groupper，传入的值最终到达averager函数中，导致当前实例终止。然后继续创建下一个实例。
+            # 如果没有group.send(None)，那么averager子生成器永远不会终止，委派生成器也永远不会在此激活，也就不会为result[key]赋值
+            group.send(None)
+        report(results)
+
+
+    # 输出报告
+    def report(results):
+        for key, result in sorted(results.items()):
+            group, unit = key.split(';')
+            print('{:2} {:5} averaging {:.2f}{}'.format(result.count, group, result.average, unit))
+
+
+    data = {
+        'girls;kg':[40, 41, 42, 43, 44, 54],
+        'girls;m': [1.5, 1.6, 1.8, 1.5, 1.45, 1.6],
+        'boys;kg':[50, 51, 62, 53, 54, 54],
+        'boys;m': [1.6, 1.8, 1.8, 1.7, 1.55, 1.6],
+    }
+
+    main(data)
 if __name__ == '__main__':
     # test_call_back_learn()
     print ('--------------')
     # test_generator_learn()
     # test_hello()
-    test_basic_yield()
+    #test_yield_return()
+    #yield_from_simple_use()
+    #yield_from_flatten()
+    use_yield_complex()
---
+++ coroutine/yield_taxi_sim.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+import random
+import collections
+import queue
+import argparse
+
+DEFAULT_NUMBER_OF_TAXIS = 3
+DEFAULT_END_TIME = 180
+SEARCH_DURATION = 5.0
+TRIP_DURATION = 20.0
+DEPARTURE_INTERAVAL = 5
+
+
+# time 是事件发生的仿真时间，proc 是出租车进程实例的编号，action是描述活动的字符串
+Event = collections.namedtuple('Event', 'time proc action')
+
+# 开始 出租车进程
+# 每辆出租车调用一次taxi_process 函数，创建一个生成器对象，表示各辆出租车的运营过程。
+def taxi_process(ident, trips, start_time=0):
+    '''
+    每次状态变化时向创建事件，把控制权交给仿真器
+    :param ident: 出租车编号
+    :param trips: 出租车回家前的行程数量
+    :param start_time: 离开车库的时间
+    :return:
+    '''
+    time = yield Event(start_time, ident, 'leave garage')  # 产出的第一个Event
+    for i in range(trips):  # 每次行程都会执行一遍这个代码块
+        # 产出一个Event实例，表示拉到了乘客 协程在这里暂停 等待下一次send() 激活
+        time = yield Event(time, ident, 'pick up passenger')
+        # 产出一个Event实例，表示乘客下车 协程在这里暂停 等待下一次send() 激活
+        time = yield Event(time, ident, 'drop off passenger')
+    # 指定的行程数量完成后，for 循环结束，最后产出 'going home' 事件。协程最后一次暂停
+    yield Event(time, ident, 'going home')
+    # 协程执行到最后 抛出StopIteration 异常
+
+
+def compute_duration(previous_action):
+    '''使用指数分布计算操作的耗时'''
+    if previous_action in ['leave garage', 'drop off passenger']:
+        # 新状态是四处徘徊
+        interval = SEARCH_DURATION
+    elif previous_action == 'pick up passenger':
+        # 新状态是开始行程
+        interval = TRIP_DURATION
+    elif previous_action == 'going home':
+        interval = 1.0
+    else:
+        raise ValueError('Unkonw previous_action: %s' % previous_action)
+
+    print ('interval: ',interval)
+    return int(random.expovariate(1 / interval)) + 1
+
+
+# 开始仿真
+class Simulator:
+    def __init__(self, procs_map):
+        self.events = queue.PriorityQueue()  # 带优先级的队列 会按时间正向排序
+        self.procs = dict(procs_map)  # 从获取的procs_map 参数中创建本地副本，为了不修改用户传入的值
+
+    def run(self, end_time):
+        '''
+        调度并显示事件，直到时间结束
+        :param end_time:  结束时间 只需要指定一个参数
+        :return:
+        '''
+        # 调度各辆出租车的第一个事件
+        for iden, proc in sorted(self.procs.items()):
+            first_event = next(proc)  # 预激协程 并产出一个 Event 对象
+            self.events.put(first_event)  # 把各个事件加到self.events 属性表示的 PriorityQueue对象中
+
+        # 此次仿真的主循环
+        sim_time = 0  # 把 sim_time 归0
+        while sim_time < end_time:
+            if self.events.empty():  # 事件全部完成后退出循环
+                print('*** end of event ***')
+                break
+            current_event = self.events.get()  # 获取优先级最高(time 属性最小)的事件
+            sim_time, proc_id, previous_action = current_event  # 更新 sim_time
+            print('taxi:', proc_id, proc_id * '  ', current_event)
+            active_proc = self.procs[proc_id]  # 从self.procs 字典中获取表示当前活动的出租车协程
+            next_time = sim_time + compute_duration(previous_action)
+            try:
+                next_event = active_proc.send(next_time)  # 把计算得到的时间发送给出租车协程。协程会产出下一个事件，或者抛出 StopIteration
+            except StopIteration:
+                del self.procs[proc_id]  # 如果有异常 表示已经退出， 删除这个协程
+            else:
+                self.events.put(next_event)  # 如果没有异常，把next_event 加入到队列
+        else:  # 如果超时 则走到这里
+            msg = '*** end of simulation time: {} event pendding ***'
+            print(msg.format(self.events.qsize()))
+
+
+def main(end_time=DEFAULT_END_TIME, num_taxis=DEFAULT_NUMBER_OF_TAXIS,
+         seed=None):
+    '''初始化随机生成器，构建过程，运行仿真程序'''
+    if seed is not None:
+        random.seed(seed)  # 获取可复现的结果
+    # 构建taxis 字典。值是三个参数不同的生成器对象。
+    taxis = {i: taxi_process(i, (i + 1) * 2, i * DEPARTURE_INTERAVAL)
+             for i in range(num_taxis)}
+
+    print ("taxis: ",taxis)
+    sim = Simulator(taxis)
+    sim.run(end_time)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Taxi fleet simulator.')
+    parser.add_argument('-e', '--end-time', type=int,
+                        default=DEFAULT_END_TIME,
+                        help='simulation end time; default=%s' % DEFAULT_END_TIME)
+    parser.add_argument('-t', '--taxis', type=int,
+                        default=DEFAULT_NUMBER_OF_TAXIS,
+                        help='number of taxis running; default = %s' % DEFAULT_NUMBER_OF_TAXIS)
+    parser.add_argument('-s', '--seed', type=int, default=None,
+                        help='random generator seed (for testing)')
+
+    args = parser.parse_args()
+    main(args.end_time, args.taxis, args.seed)
